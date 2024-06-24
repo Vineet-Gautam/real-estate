7cb409c4fe16d6ca7df266ca6fabf77ed11cae98
Vineet Gautam <vineetgautamm@gmail.com>
2024-06-24 21:09:05 +0530

    connected to db

--- api/index.js
+++ api/index.js
@@ -1,14 +1,21 @@
-import { log } from "console";
 import express from "express";
-
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+dotenv.config();
 const app = express();
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-})
+  console.log("Server is running on port 3000");
+});
 
-app.get('/', (req, res) => {
-    res.json({
-        "messgae": "hi",
-    })
-})
+mongoose.connect("mongodb+srv://vineet:7qRlebfF0mbtEKZs@real-estate.wv43sow.mongodb.net/real-estate?retryWrites=true&w=majority&appName=real-estate").then((result) => {
+    console.log("connected to mongo db");
+}).catch((err) => {
+    console.log("error");
+});
+
+app.get("/", (req, res) => {
+  res.json({
+    messgae: "hi",
+  });
+});
